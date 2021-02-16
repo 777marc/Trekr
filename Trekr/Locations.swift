a205bc0872b746e41826078bfff77e31fdff8118
Marc Mendoza <777marc@gmail.com>
2021-02-16 18:23:13 -0500

    init
    init

---
+++ Trekr/Locations.swift
@@ -0,0 +1,26 @@
+//
+//  Locations.swift
+//  Trekr
+//
+//  Created by Marc Mendoza on 2/16/21.
+//
+
+import Foundation
+
+class Locations : ObservableObject {
+    
+    let places: [Location]
+    
+    var primary: Location {
+        places[0]
+    }
+    
+    init() {
+        let url = Bundle.main.url(forResource: "locations", withExtension: "json")!
+        
+        let data = try! Data(contentsOf: url)
+        
+        places = try! JSONDecoder().decode([Location].self, from: data)
+    }
+    
+}

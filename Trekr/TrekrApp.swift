5214bc0a2b15dcd682ba94793825cad70ac527e6
Marc Mendoza <777marc@gmail.com>
2021-02-16 11:11:46 -0500

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Marc Mendoza on 2/16/21.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
